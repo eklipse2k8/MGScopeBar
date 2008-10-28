9e5d11f54d65cbdf1918bf945ac7a07a333630c9
mattgemmell <mattgemmell@fad7f400-0e54-0410-bf1d-e368f886e4d4>
2008-10-28 16:32:35 +0000

    Initial checkin of MGScopeBar.
    git-svn-id: http://svn.cocoasourcecode.com/MGScopeBar@19 fad7f400-0e54-0410-bf1d-e368f886e4d4

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MGScopeBar
+//
+//  Created by Matt Gemmell on 28/09/2008.
+//  Copyright Instinctive Code 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
